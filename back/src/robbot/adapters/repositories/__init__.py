14d6175c918a4e26dcc3e0e6341a11983442ef8d
edyoCampos <camposevero@gmail.com>
2025-12-26 18:54:28 -0300

    refactor: refatora __init__.py

--- back/src/robbot/adapters/repositories/__init__.py
+++ back/src/robbot/adapters/repositories/__init__.py
@@ -1,1 +1,11 @@
-__all__ = []
+"""Repository exports for dependency injection."""
+
+from robbot.adapters.repositories.auth_session_repository import AuthSessionRepository
+from robbot.adapters.repositories.conversation_repository import ConversationRepository
+from robbot.adapters.repositories.credential_repository import CredentialRepository
+
+__all__ = [
+    "AuthSessionRepository",
+    "ConversationRepository",
+    "CredentialRepository",
+]
