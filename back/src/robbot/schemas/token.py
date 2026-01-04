5dd378c02b21b278c4aa54e5fae0f335c067c007
edyoCampos <camposevero@gmail.com>
2026-01-04 01:41:27 -0300

    refactor: refatora token.py

--- back/src/robbot/schemas/token.py
+++ back/src/robbot/schemas/token.py
@@ -1,13 +1,15 @@
-from typing import Optional
+
 from pydantic import BaseModel
 
 
 class Token(BaseModel):
     access_token: str
     refresh_token: str
-    token_type: str
+    token_type: str = "bearer"
+    mfa_required: bool = False
+    user: object | None = None  # UserModel object
 
 
 class TokenData(BaseModel):
-    refresh_token: Optional[str] = None
-    access_token: Optional[str] = None
+    refresh_token: str | None = None
+    access_token: str | None = None
