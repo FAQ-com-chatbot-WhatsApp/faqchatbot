9e16174b2fe6159b5452c41feba9998c1294d993
edyoCampos <camposevero@gmail.com>
2026-01-04 02:19:28 -0300

    refactor: refatora conversation_message_model.py

--- back/src/robbot/infra/db/models/conversation_message_model.py
+++ back/src/robbot/infra/db/models/conversation_message_model.py
@@ -13,8 +13,6 @@ from robbot.infra.db.base import Base
 
 if TYPE_CHECKING:
     from robbot.infra.db.models.conversation_model import ConversationModel
-
-
 class ConversationMessageModel(Base):
     """Model for messages within conversations.
 
