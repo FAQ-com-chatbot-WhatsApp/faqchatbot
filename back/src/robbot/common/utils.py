03218779dc40f6d4b06ee489058b66899e35cc08
edyoCampos <camposevero@gmail.com>
2026-01-17 22:03:44 -0300

    refactor: refatora utils.py

--- back/src/robbot/common/utils.py
+++ back/src/robbot/common/utils.py
@@ -68,7 +68,7 @@ def send_email(to: str, subject: str, body: str) -> None:
                 server.login(user, password)
             server.sendmail(sender, [to], msg.as_string())
         logger.info("[SUCCESS] Email sent to %s: %s", to, subject)
-    except Exception as exc:  # noqa: BLE001 (blind exception)
+    except Exception as exc:  # noqa: BLE001 (blind exception)  # pylint: disable=broad-exception-caught
         logger.exception("[ERROR] Failed to send email to %s: %s", to, exc)
 
 
