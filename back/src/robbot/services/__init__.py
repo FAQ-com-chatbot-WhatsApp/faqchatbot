07077572c7f4d6b6276f4bd41baa6b1144c58571
edyoCampos <camposevero@gmail.com>
2026-02-06 15:43:56 -0300

    refactor: refatora __init__.py

--- back/src/robbot/services/__init__.py
+++ back/src/robbot/services/__init__.py
@@ -1,1 +1,5 @@
-__all__ = []
+from .bot.conversation_orchestrator import ConversationOrchestrator
+from .bot.conversation_pipeline import ConversationPipeline
+from .bot.conversation_service import ConversationService
+from .response_dispatcher import ResponseDispatcher
+from .leads.lead_service import LeadService
