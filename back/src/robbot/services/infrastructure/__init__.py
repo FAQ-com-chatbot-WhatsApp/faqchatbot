4d9354d93ea550b57a407311cd073538839cc1fa
edyoCampos <camposevero@gmail.com>
2026-02-09 14:48:00 -0300

    refactor: refatora __init__.py

--- back/src/robbot/services/infrastructure/__init__.py
+++ back/src/robbot/services/infrastructure/__init__.py
@@ -1,13 +1,1 @@
-"""Infrastructure services module."""
-
-from robbot.services.infrastructure.export_service import ExportService
-from robbot.services.infrastructure.health_service import HealthService
-from robbot.services.infrastructure.queue_service import QueueService
-from robbot.services.infrastructure.worker_analytics_service import WorkerAnalyticsService
-
-__all__ = [
-    "ExportService",
-    "HealthService",
-    "QueueService",
-    "WorkerAnalyticsService",
-]
+"""Package."""
