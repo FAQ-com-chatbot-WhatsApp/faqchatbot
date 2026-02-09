cdf98820e78d01e726a433b099ecb51a88ec6b50
edyoCampos <camposevero@gmail.com>
2026-02-09 10:26:38 -0300

    refactor: refatora __init__.py

--- back/src/robbot/services/bot/__init__.py
+++ back/src/robbot/services/bot/__init__.py
@@ -1,6 +1,19 @@
-# Bot orchestration services
-from .conversation_orchestrator import ConversationOrchestrator
-from .conversation_pipeline import ConversationPipeline
-from .conversation_service import ConversationService
-from .message_pipeline import MessagePipeline
-from .conversation_state_machine import ConversationStateMachine
+"""Bot orchestration services."""
+
+from robbot.services.bot.conversation_orchestrator import ConversationOrchestrator
+from robbot.services.bot.conversation_pipeline import ConversationPipeline
+from robbot.services.bot.conversation_service import ConversationService
+from robbot.services.bot.conversation_state_machine import ConversationStateMachine
+from robbot.services.bot.message_pipeline import MessagePipeline
+from robbot.services.bot.response_dispatcher import ResponseDispatcher
+from robbot.services.bot.response_generator import ResponseGenerator
+
+__all__ = [
+    "ConversationOrchestrator",
+    "ConversationPipeline",
+    "ConversationService",
+    "ConversationStateMachine",
+    "MessagePipeline",
+    "ResponseDispatcher",
+    "ResponseGenerator",
+]
