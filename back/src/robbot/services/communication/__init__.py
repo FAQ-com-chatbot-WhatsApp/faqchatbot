e66d5044b300d77d67d461087e63b261794de9e8
edyoCampos <camposevero@gmail.com>
2026-02-09 10:26:45 -0300

    refactor: refatora __init__.py

--- back/src/robbot/services/communication/__init__.py
+++ back/src/robbot/services/communication/__init__.py
@@ -1,3 +1,11 @@
-from .transcription_service import TranscriptionService
-from .text_sanitizer import enforce_whatsapp_style
-from .message_processor import MessageProcessor
+"""Communication services module."""
+
+from robbot.services.communication.message_processor import MessageProcessor
+from robbot.services.communication.text_sanitizer import enforce_whatsapp_style
+from robbot.services.communication.transcription_service import TranscriptionService
+
+__all__ = [
+    "MessageProcessor",
+    "TranscriptionService",
+    "enforce_whatsapp_style",
+]
