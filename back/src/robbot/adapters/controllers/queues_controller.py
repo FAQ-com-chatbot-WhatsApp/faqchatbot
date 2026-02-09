13a49b04a5f84189663119e61d78f84da65933f9
edyoCampos <camposevero@gmail.com>
2026-02-09 10:25:54 -0300

    refactor: refatora queues_controller.py

--- back/src/robbot/adapters/controllers/queues_controller.py
+++ back/src/robbot/adapters/controllers/queues_controller.py
@@ -6,7 +6,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from robbot.api.v1.dependencies import get_current_user, get_db
-from robbot.services.queue_service import get_queue_service
+from robbot.services.infrastructure.queue_service import get_queue_service
 
 router = APIRouter()
 
