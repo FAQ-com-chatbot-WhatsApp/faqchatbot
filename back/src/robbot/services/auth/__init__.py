be1150de423efb15bb3416aaaea636411312848a
edyoCampos <camposevero@gmail.com>
2026-03-12 17:17:15 -0300

    refactor: refatora __init__.py

--- back/src/robbot/services/auth/__init__.py
+++ back/src/robbot/services/auth/__init__.py
@@ -1,7 +1,7 @@
 """Auth services module."""
 
-from robbot.services.auth.auth_services import AuthService
 from robbot.services.auth.audit_service import AuditService
+from robbot.services.auth.auth_services import AuthService
 from robbot.services.auth.credential_service import CredentialService
 from robbot.services.auth.email_verification_service import EmailVerificationService
 from robbot.services.auth.mfa_service import MfaService
