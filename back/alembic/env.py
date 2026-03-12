9f216f66a9c77031bbb73a0120394c4d4ed69b8a
edyoCampos <camposevero@gmail.com>
2026-03-12 17:15:01 -0300

    refactor: refatora env.py

--- back/alembic/env.py
+++ back/alembic/env.py
@@ -7,8 +7,8 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config, pool
 
 from alembic import context
-from robbot.infra.persistence import models  # noqa: F401 - import to register models
 from robbot.infra.db.base import Base
+from robbot.infra.persistence import models  # noqa: F401 - import to register models
 
 # this is the Alembic Config object, which provides access to the values within the .ini file
 config = context.config  # type: ignore
