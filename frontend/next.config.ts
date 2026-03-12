e4726c02a66cab049cf58113513e59d8bd309d7e
edyoCampos <camposevero@gmail.com>
2026-03-12 13:49:16 -0300

    refactor: refatora next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/waha/:path*',
+        destination: 'http://waha:3000/api/:path*',
+      },
+    ]
+  },
 }
 
 export default nextConfig
