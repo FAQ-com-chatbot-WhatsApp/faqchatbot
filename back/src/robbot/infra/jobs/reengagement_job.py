d0a525cd4837444f6475349b27afd5c0fb609a9a
edyoCampos <camposevero@gmail.com>
2026-03-12 17:16:11 -0300

    refactor: refatora reengagement_job.py

--- back/src/robbot/infra/jobs/reengagement_job.py
+++ back/src/robbot/infra/jobs/reengagement_job.py
@@ -11,13 +11,13 @@ import asyncio
 import logging
 from datetime import UTC, datetime, timedelta
 
-from robbot.infra.integrations.waha.waha_client import WAHAClient
-from robbot.infra.persistence.repositories.conversation_message_repository import ConversationMessageRepository
-from robbot.infra.persistence.repositories.conversation_repository import ConversationRepository
 from robbot.core.custom_exceptions import DatabaseError, JobError, WAHAError
 from robbot.domain.shared.enums import ConversationStatus, MessageDirection
-from robbot.infra.persistence.models.conversation_message_model import ConversationMessageModel as ConversationMessage
 from robbot.infra.db.session import SessionLocal
+from robbot.infra.integrations.waha.waha_client import WAHAClient
+from robbot.infra.persistence.models.conversation_message_model import ConversationMessageModel as ConversationMessage
+from robbot.infra.persistence.repositories.conversation_message_repository import ConversationMessageRepository
+from robbot.infra.persistence.repositories.conversation_repository import ConversationRepository
 
 logger = logging.getLogger(__name__)
 
@@ -217,4 +217,3 @@ def run_reengagement_job():
     """
     job = ReEngagementJob()
     return job.execute()
-
