d3a8141493d62573e42a77e60c5a4eeb1eb60089
edyoCampos <camposevero@gmail.com>
2026-03-31 00:13:21 -0300

    refactor: refatora Sidebar.tsx

--- frontend/src/components/dashboard/Sidebar.tsx
+++ frontend/src/components/dashboard/Sidebar.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { usePathname } from 'next/navigation'
-import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 import { Home, Users, MessageSquare, Settings, Library, Bell } from 'lucide-react'
 
